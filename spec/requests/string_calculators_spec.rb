aeb2d08ff47aae71d3e2788a0e4db1592e1fece6
Abhinav Mishra <abhinavmishra@Abhinavs-MacBook-Air.local>
2024-12-18 16:20:26 +0530

    Added specs for StringCalculatorsController

--- spec/requests/string_calculators_spec.rb
+++ spec/requests/string_calculators_spec.rb
@@ -3,15 +3,22 @@ require 'rails_helper'
 RSpec.describe "StringCalculators", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/string_calculators/index"
+      get '/'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /calculate" do
     it "returns http success" do
-      get "/string_calculators/calculate"
+      get "/string_calculators/calculate", params: { numbers: "1,2,3" }
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #calculate" do
+    it "calculates the sum of valid numbers" do
+      get "/string_calculators/calculate", params: { numbers: "1,2,3" }
+      expect(response.body).to include("Result: 6")
     end
   end
 
