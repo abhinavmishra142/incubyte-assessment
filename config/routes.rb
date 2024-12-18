131bd9b3252265028b44b79722ab54665d31a2e9
Abhinav Mishra <abhinavmishra@Abhinavs-MacBook-Air.local>
2024-12-18 15:59:41 +0530

    Added UI for sring calculation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'string_calculators/index'
+  get 'string_calculators/calculate'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +12,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "string_calculators#calculate"
 end
