68bbdb02b4a676a6ede90cdc0da7dec47359ca34
Abhinav Mishra <abhinavmishra@Abhinavs-MacBook-Air.local>
2024-12-18 15:39:32 +0530

    updated string calculator spec

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -2,33 +2,29 @@ require 'rails_helper'
 
 RSpec.describe StringCalculator, type: :model do
   describe '#add' do
+    let(:calculator) { StringCalculator.new }
+  
     it 'returns 0 for number as a blank string' do
-      calculator = StringCalculator.new
       expect(calculator.add('')).to eq(0)
     end
 
     it 'It should return self number for a single number' do
-      calculator = StringCalculator.new
       expect(calculator.add('1')).to eq(1)
     end
 
     it 'It should return the sum of two comma separated numbers' do
-      calculator = StringCalculator.new
       expect(calculator.add('1,5')).to eq(6)
     end
 
     it 'It should supports newlines and comma separator' do
-      calculator = StringCalculator.new
       expect(calculator.add('1\n2,3')).to eq(6)
     end
 
     it 'It should supports custom delimiter' do
-      calculator = StringCalculator.new
       expect(calculator.add("//;\n1;2")).to eq(3)
     end
 
     it 'It should not alow negative numbers' do
-      calculator = StringCalculator.new
       expect { calculator.add("1,-2,3,-4") }
         .to raise_error("negative numbers not allowed -2, -4")
     end
